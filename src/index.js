b889374ee7119c4cf0e4b9294bea32850e01dccb
Yevheniia Tobolovska <tobolovskaya@gmail.com>
2025-03-18 12:42:58 +0100

    Fix contactsPath and working CLI

--- src/index.js
+++ src/index.js
@@ -1,37 +1,56 @@
 import { program } from "commander";
+import { listContacts, getContactById, addContact, removeContact } from "./contacts.js";
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
 
 program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
-async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+async function invokeAction({ action, id, ...data }) {
+    switch (action) {
+        case "list":
+            await listContacts()
+                .then(contacts => { console.table(contacts); })
+                .catch(err => { console.error("Error getting contacts list: ", err.message); });
+            break;
+        case "get":
+            await getContactById(id)
+                .then(contact => {
+                    if (contact) {
+                        console.log(contact);
+                    } else {
+                        console.warn("\x1B[31m Contact with id:", id, "not found!");
+                    }
+                })
+                .catch(err => { console.error("\x1B[31m Error getting contact by ID:", err.message); });
+            break;
 
-    case "get":
-      // ... id
-      break;
+        case "add":
+            await addContact(data)
+                .then(data => { console.log(data); })
+                .catch(err => { console.error("\x1B[31m Error adding contact:", err.message); });
+            break;
 
-    case "add":
-      // ... name email phone
-      break;
+        case "remove":
+            await removeContact(id)
+                .then(contact => {
+                    if (contact) {
+                        console.log(contact);
+                    } else {
+                        console.warn("\x1B[31m Contact with id:", id, "not found!");
+                    }
+                })
+                .catch(err => { console.error("\x1B[31m Error removing contact:", err.message); });
+            break;
 
-    case "remove":
-      // ... id
-      break;
-
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
 }
 
 invokeAction(options);
